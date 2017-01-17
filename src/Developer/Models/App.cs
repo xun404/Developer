30966edb5bbfb41f1cdb374254a69aa80d57e04a
anduin <anduin@aiursoft.com>
2017-01-17 20:21:49 +0800

    now what

---
+++ src/Developer/Models/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Developer.Models
+{
+    public class App
+    {
+        public virtual string AppId { get; set; }
+        public virtual string AppSecret { get; set; }
+
+        public virtual string AppName { get; set; }
+
+        [ForeignKey(nameof(OwnerId))]
+        public virtual DeveloperUser Owner { get; set; }
+        public virtual string OwnerId { get; set; }
+    }
+}
---
+++ src/Developer/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Developer.Models
+{
+    public class DeveloperUser : IdentityUser
+    {
+        [InverseProperty(nameof(App.Owner))]
+        public virtual List<App> MyApps { get; set; } = new List<App>();
+    }
+}
